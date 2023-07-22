e810c7447f7f18f1cb2e2def4d6866693b1d538c
kevinrmartinez <kevinrmartinezj@gmail.com>
2023-07-22 17:47:39 -0400

    Inicio de Database

---
+++ DataAccess/DBcont.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class DBcont:DbContext
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=database.db");
+        }
+    }
+}
