0696931cd58a5c2cfba5e062077e519e17115e94
Lisbeth Diaz <lizbeths.robles@gmail.com>
2023-08-03 03:08:33 -0400

    Some funtions added

---
+++ BusinessLogic/EmpleadosService.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class EmpleadosService
+    {
+        private readonly DBcont _context;
+
+        public EmpleadosService()
+        {
+            var context = new DBcont();
+            _context = context;
+        }
+
+        public async Task<List<empleados>> GetEmpleados()
+        {
+            var empleados = await _context.empleados.ToListAsync();
+            return empleados;
+        }
+
+
+    }
+}
---
+++ BusinessLogic/MedicosDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class MedicosDto
+    {
+        
+        public string? Nombre { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public string? Poblacion { get; set; }
+        public string? Provincia { get; set; }
+        public int Codigo_Postal { get; set; }
+        public int NIF { get; set; }
+        public string? Numero_de_seguridad_social { get; set; }
+        public string? Numero_de_colegiado { get; set; }
+        public int Tipo_empleado { get; set; }
+        public bool EsSustituto { get; set; }
+        public bool Activo { get; set; }
+    }
+}
