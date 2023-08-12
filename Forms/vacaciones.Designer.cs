0967114f616c9dd3d9ec9ee61b22607ea7c3ade3
Fabio Andres <arvelofabio2@gmail.com>
2023-08-12 15:10:25 -0400

    vacaciones

--- Forms/vacaciones.Designer.cs
+++ Forms/vacaciones.Designer.cs
@@ -33,26 +33,6 @@
             panel2 = new Panel();
             label14 = new Label();
             labelID = new Label();
-            label12 = new Label();
-            label11 = new Label();
-            textBox9 = new TextBox();
-            label10 = new Label();
-            textBox11 = new TextBox();
-            textBox10 = new TextBox();
-            numericUpDown2 = new NumericUpDown();
-            label9 = new Label();
-            textBox5 = new TextBox();
-            label6 = new Label();
-            label5 = new Label();
-            textBox4 = new TextBox();
-            numericUpDown1 = new NumericUpDown();
-            label8 = new Label();
-            label4 = new Label();
-            textBox3 = new TextBox();
-            label7 = new Label();
-            textBox2 = new TextBox();
-            label13 = new Label();
-            textBox1 = new TextBox();
             panel2_2 = new Panel();
             btnNOCambios = new Button();
             btnOKCambios = new Button();
@@ -70,11 +50,13 @@
             button2 = new Button();
             label1 = new Label();
             dataGridView1 = new DataGridView();
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker1 = new DateTimePicker();
+            label4 = new Label();
+            label6 = new Label();
             panel1.SuspendLayout();
             panelBottom.SuspendLayout();
             panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             panel2_2.SuspendLayout();
             panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -94,7 +76,7 @@
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(883, 500);
+            panel1.Size = new Size(879, 500);
             panel1.TabIndex = 1;
             panel1.Paint += panel1_Paint;
             // 
@@ -105,39 +87,23 @@
             panelBottom.Dock = DockStyle.Bottom;
             panelBottom.Location = new Point(0, 265);
             panelBottom.Name = "panelBottom";
-            panelBottom.Size = new Size(883, 201);
+            panelBottom.Size = new Size(879, 201);
             panelBottom.TabIndex = 31;
             // 
             // panel2
             // 
+            panel2.Controls.Add(dateTimePicker2);
+            panel2.Controls.Add(dateTimePicker1);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label6);
             panel2.Controls.Add(label14);
             panel2.Controls.Add(labelID);
-            panel2.Controls.Add(label12);
-            panel2.Controls.Add(label11);
-            panel2.Controls.Add(textBox9);
-            panel2.Controls.Add(label10);
-            panel2.Controls.Add(textBox11);
-            panel2.Controls.Add(textBox10);
-            panel2.Controls.Add(numericUpDown2);
-            panel2.Controls.Add(label9);
-            panel2.Controls.Add(textBox5);
-            panel2.Controls.Add(label6);
-            panel2.Controls.Add(label5);
-            panel2.Controls.Add(textBox4);
-            panel2.Controls.Add(numericUpDown1);
-            panel2.Controls.Add(label8);
-            panel2.Controls.Add(label4);
-            panel2.Controls.Add(textBox3);
-            panel2.Controls.Add(label7);
-            panel2.Controls.Add(textBox2);
-            panel2.Controls.Add(label13);
-            panel2.Controls.Add(textBox1);
             panel2.Controls.Add(panel2_2);
             panel2.Controls.Add(label2);
             panel2.Dock = DockStyle.Bottom;
             panel2.Location = new Point(0, 0);
             panel2.Name = "panel2";
-            panel2.Size = new Size(883, 158);
+            panel2.Size = new Size(879, 158);
             panel2.TabIndex = 29;
             panel2.Visible = false;
             // 
@@ -146,7 +112,7 @@
             label14.AutoSize = true;
             label14.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             label14.ForeColor = Color.White;
-            label14.Location = new Point(584, 66);
+            label14.Location = new Point(616, 83);
             label14.Name = "label14";
             label14.Size = new Size(28, 21);
             label14.TabIndex = 77;
@@ -157,198 +123,18 @@
             labelID.AutoSize = true;
             labelID.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             labelID.ForeColor = Color.White;
-            labelID.Location = new Point(618, 66);
+            labelID.Location = new Point(650, 83);
             labelID.Name = "labelID";
             labelID.Size = new Size(58, 21);
             labelID.TabIndex = 76;
             labelID.Text = "labelID";
-            // 
-            // label12
-            // 
-            label12.AutoSize = true;
-            label12.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label12.ForeColor = Color.White;
-            label12.Location = new Point(559, 3);
-            label12.Name = "label12";
-            label12.Size = new Size(30, 15);
-            label12.TabIndex = 68;
-            label12.Text = "Tipo";
-            // 
-            // label11
-            // 
-            label11.AutoSize = true;
-            label11.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label11.ForeColor = Color.White;
-            label11.Location = new Point(381, 100);
-            label11.Name = "label11";
-            label11.Size = new Size(124, 15);
-            label11.TabIndex = 75;
-            label11.Text = "Numero de Colegiado";
-            // 
-            // textBox9
-            // 
-            textBox9.Location = new Point(559, 21);
-            textBox9.Name = "textBox9";
-            textBox9.Size = new Size(139, 23);
-            textBox9.TabIndex = 63;
-            // 
-            // label10
-            // 
-            label10.AutoSize = true;
-            label10.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label10.ForeColor = Color.White;
-            label10.Location = new Point(381, 50);
-            label10.Name = "label10";
-            label10.Size = new Size(157, 15);
-            label10.TabIndex = 74;
-            label10.Text = "Numero de Seguridad Social";
-            // 
-            // textBox11
-            // 
-            textBox11.Location = new Point(381, 68);
-            textBox11.Name = "textBox11";
-            textBox11.Size = new Size(160, 23);
-            textBox11.TabIndex = 73;
-            // 
-            // textBox10
-            // 
-            textBox10.Location = new Point(381, 118);
-            textBox10.Name = "textBox10";
-            textBox10.Size = new Size(160, 23);
-            textBox10.TabIndex = 72;
-            // 
-            // numericUpDown2
-            // 
-            numericUpDown2.Location = new Point(381, 21);
-            numericUpDown2.Name = "numericUpDown2";
-            numericUpDown2.Size = new Size(139, 23);
-            numericUpDown2.TabIndex = 71;
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label9.ForeColor = Color.White;
-            label9.Location = new Point(381, 3);
-            label9.Name = "label9";
-            label9.Size = new Size(25, 15);
-            label9.TabIndex = 70;
-            label9.Text = "NIF";
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(214, 68);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(139, 23);
-            textBox5.TabIndex = 69;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.ForeColor = Color.White;
-            label6.Location = new Point(214, 50);
-            label6.Name = "label6";
-            label6.Size = new Size(56, 15);
-            label6.TabIndex = 68;
-            label6.Text = "Provincia";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.ForeColor = Color.White;
-            label5.Location = new Point(214, 3);
-            label5.Name = "label5";
-            label5.Size = new Size(60, 15);
-            label5.TabIndex = 67;
-            label5.Text = "Poblacion";
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(214, 21);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(139, 23);
-            textBox4.TabIndex = 66;
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new Point(214, 119);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(120, 23);
-            numericUpDown1.TabIndex = 62;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label8.ForeColor = Color.White;
-            label8.Location = new Point(214, 100);
-            label8.Name = "label8";
-            label8.Size = new Size(81, 15);
-            label8.TabIndex = 65;
-            label8.Text = "Codigo postal";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.ForeColor = Color.White;
-            label4.Location = new Point(3, 100);
-            label4.Name = "label4";
-            label4.Size = new Size(52, 15);
-            label4.TabIndex = 64;
-            label4.Text = "Telefono";
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(3, 118);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(176, 23);
-            textBox3.TabIndex = 63;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label7.ForeColor = Color.White;
-            label7.Location = new Point(3, 50);
-            label7.Name = "label7";
-            label7.Size = new Size(57, 15);
-            label7.TabIndex = 58;
-            label7.Text = "Direccion";
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(3, 68);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(176, 23);
-            textBox2.TabIndex = 57;
-            // 
-            // label13
-            // 
-            label13.AutoSize = true;
-            label13.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label13.ForeColor = Color.White;
-            label13.Location = new Point(3, 3);
-            label13.Name = "label13";
-            label13.Size = new Size(51, 15);
-            label13.TabIndex = 56;
-            label13.Text = "Nombre";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(3, 21);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(176, 23);
-            textBox1.TabIndex = 55;
             // 
             // panel2_2
             // 
             panel2_2.Controls.Add(btnNOCambios);
             panel2_2.Controls.Add(btnOKCambios);
             panel2_2.Dock = DockStyle.Right;
-            panel2_2.Location = new Point(812, 0);
+            panel2_2.Location = new Point(808, 0);
             panel2_2.Name = "panel2_2";
             panel2_2.Size = new Size(71, 158);
             panel2_2.TabIndex = 1;
@@ -404,7 +190,7 @@
             panel3.Dock = DockStyle.Bottom;
             panel3.Location = new Point(0, 158);
             panel3.Name = "panel3";
-            panel3.Size = new Size(883, 43);
+            panel3.Size = new Size(879, 43);
             panel3.TabIndex = 28;
             panel3.Visible = false;
             // 
@@ -416,7 +202,7 @@
             btnOKDelete.FlatStyle = FlatStyle.Flat;
             btnOKDelete.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnOKDelete.ForeColor = Color.White;
-            btnOKDelete.Location = new Point(723, 0);
+            btnOKDelete.Location = new Point(719, 0);
             btnOKDelete.Name = "btnOKDelete";
             btnOKDelete.Size = new Size(79, 43);
             btnOKDelete.TabIndex = 81;
@@ -431,7 +217,7 @@
             btnNODelete.FlatStyle = FlatStyle.Flat;
             btnNODelete.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnNODelete.ForeColor = Color.White;
-            btnNODelete.Location = new Point(802, 0);
+            btnNODelete.Location = new Point(798, 0);
             btnNODelete.Name = "btnNODelete";
             btnNODelete.Size = new Size(81, 43);
             btnNODelete.TabIndex = 80;
@@ -475,7 +261,7 @@
             panelSuperBottom.Dock = DockStyle.Bottom;
             panelSuperBottom.Location = new Point(0, 466);
             panelSuperBottom.Name = "panelSuperBottom";
-            panelSuperBottom.Size = new Size(883, 34);
+            panelSuperBottom.Size = new Size(879, 34);
             panelSuperBottom.TabIndex = 30;
             // 
             // button1
@@ -562,6 +348,44 @@
             dataGridView1.Size = new Size(628, 178);
             dataGridView1.TabIndex = 0;
             // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Format = DateTimePickerFormat.Short;
+            dateTimePicker2.Location = new Point(394, 50);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(111, 23);
+            dateTimePicker2.TabIndex = 82;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(116, 50);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(111, 23);
+            dateTimePicker1.TabIndex = 81;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(394, 26);
+            label4.Name = "label4";
+            label4.Size = new Size(76, 21);
+            label4.TabIndex = 80;
+            label4.Text = "Fecha fin ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(116, 26);
+            label6.Name = "label6";
+            label6.Size = new Size(91, 21);
+            label6.TabIndex = 78;
+            label6.Text = "Fecha inicio";
+            // 
             // vacaciones
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -577,8 +401,6 @@
             panelBottom.ResumeLayout(false);
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             panel2_2.ResumeLayout(false);
             panel3.ResumeLayout(false);
             panel3.PerformLayout();
@@ -599,26 +421,6 @@
         private Panel panel2;
         private Label label14;
         private Label labelID;
-        private Label label12;
-        private Label label11;
-        private TextBox textBox9;
-        private Label label10;
-        private TextBox textBox11;
-        private TextBox textBox10;
-        private NumericUpDown numericUpDown2;
-        private Label label9;
-        private TextBox textBox5;
-        private Label label6;
-        private Label label5;
-        private TextBox textBox4;
-        private NumericUpDown numericUpDown1;
-        private Label label8;
-        private Label label4;
-        private TextBox textBox3;
-        private Label label7;
-        private TextBox textBox2;
-        private Label label13;
-        private TextBox textBox1;
         private Panel panel2_2;
         private Button btnNOCambios;
         private Button btnOKCambios;
@@ -630,5 +432,9 @@
         private Label labelDelID;
         private Label label3;
         private Panel panelSuperBottom;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private Label label4;
+        private Label label6;
     }
 }
