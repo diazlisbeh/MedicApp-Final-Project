9a45a4834c0d8932dcfa7a34702b588812fd488f
kevinrmartinez <kevinrmartinezj@gmail.com>
2023-08-12 16:52:56 -0400

    FinalFinal

--- DataAccess/empleados.cs
+++ DataAccess/empleados.cs
@@ -13,9 +13,9 @@ namespace DataAccess
     {
         [Key]
         public int ID { get; set; }
-        [MaxLength(10, ErrorMessage = "El nombre debe tener menos de 10 caracteres")]
+        [MaxLength(30, ErrorMessage = "El nombre debe tener menos de 30 caracteres")]
         public string? Nombre { get; set; }
-        [MaxLength(30, ErrorMessage = "La direccion debe tener menos de 30 caracteres")]
+        [MaxLength(50, ErrorMessage = "La direccion debe tener menos de 30 caracteres")]
         public string? Direccion { get; set; }
         [RegularExpression(@"^(809|829|849)-\d{3}-\d{4}$", ErrorMessage = "Formato de telefono debe ser XXX-XXX-XXXX")]
         public string? Telefono { get; set; }
--- DataAccess/pacientes.cs
+++ DataAccess/pacientes.cs
@@ -15,9 +15,9 @@ namespace DataAccess
         [IgnoreDataMember]
         public int ID { get; set; }
         [Required]
-        [MaxLength(10,ErrorMessage ="El nombre debe tener menos de 10 caracteres")]
+        [MaxLength(30,ErrorMessage ="El nombre debe tener menos de 30 caracteres")]
         public string? Nombre { get; set; }
-        [MaxLength(30, ErrorMessage = "La direccion debe tener menos de 30 caracteres")]
+        [MaxLength(50, ErrorMessage = "La direccion debe tener menos de 50 caracteres")]
         public string? Direccion { get; set; }
         [RegularExpression(@"^(809|829|849)-\d{3}-\d{4}$", ErrorMessage ="Formato de telefono debe ser XXX-XXX-XXXX")]
         public string? Telefono { get; set; }
